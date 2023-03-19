922c39d7c2235706cc827fd003def4dfbe931dfd
eduardoqsilva <eduadsilva@outlook.com>
2023-03-18 21:06:11 -0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { Background } from "./components/Background"
 import { Search } from "./components/Search"
-
+import { Wrapper } from "./components/Wrapper"
+import logoName from "./assets/logoName/comicly.png"
+import { Logo } from "./components/logo"
 
 function App() {
 
   return (
     <>
+      <Logo />
       <Background>
-        <Search /> 
+        <Wrapper>
+          <img src={logoName} />
+          <Search /> 
+        </Wrapper>
       </Background>
     </>
   )
