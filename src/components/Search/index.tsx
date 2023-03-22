da1ed44782492d5cfe4290beb47b89b1d15ab3c1
eduardoqsilva <eduadsilva@outlook.com>
2023-03-22 00:05:27 -0300

    update search input function

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,11 +1,28 @@
 import { SearchWrapper } from "./Search.styled";
 import {MagnifyingGlass} from "@phosphor-icons/react"
+import { KeyboardEvent, useState } from "react"
 
-export function Search() {
+interface type {
+  setSearch: (value:string) => void
+}
+
+export function Search({setSearch}:type) {
+  
+  function handleSetValue(e: KeyboardEvent<HTMLInputElement>) {
+    const value = e.currentTarget.value.trim()
+    
+    if (e.key === 'Enter') {
+      console.log('enter aqui')
+      if(value.length >= 3) {
+        setSearch(value)
+        e.currentTarget.value = ''
+      }
+    }
+  }
   return(
     <SearchWrapper>
       <MagnifyingGlass color="#0D9BEB" size={32} weight={"bold"}/>
-      <input type="text" placeholder="look for your hero here"/> 
+      <input onKeyDown={handleSetValue} type="text" placeholder="look for your hero here"/> 
     </SearchWrapper>
   )
 }
