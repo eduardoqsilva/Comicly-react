92a188b64344b40660f0a74659005dd3962595da
eduardoqsilva <eduadsilva@outlook.com>
2023-03-22 00:04:39 -0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,50 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
 import { Background } from "./components/Background"
 import { Search } from "./components/Search"
 import { Wrapper } from "./components/Wrapper"
 import logoName from "./assets/logoName/comicly.png"
 import { Logo } from "./components/Logo"
 import { Overlay } from "./components/Overlay"
-import { useState } from "react"
+
+
+
+const key = import.meta.env.VITE_API_KEY
 
 function App() {
-
-  const [show, setShow] = useState(true)
+  
+  const [show, setShow] = useState(false)
+  const [isFetching, setIsFetching] = useState(true)
+  const [data, setData] = useState<object>()
+  const [search, setSearch] = useState<string>()
+  const url = `https://www.superheroapi.com/api.php/${key}/search/${search}`
 
   function handleSetShow() {
     setShow((prev) => !prev)
   }
+  function handleSetSearch(text:string) {
+    setSearch(text)
+    setShow(true)
+  }
+
+  useEffect(() =>{
+    axios.get(url)
+    .then((res) => {
+      setData(res.data.results)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+    .finally(() => {
+      setIsFetching(false)
+    })
+    console.log(search)
+  },[search])
+
+  useEffect(() => {
+    console.log(data)
+  },[data])
 
   return (
     <>
@@ -20,10 +52,11 @@ function App() {
       <Background>
         <Wrapper>
           <img src={logoName} />
-          <Search /> 
-        </Wrapper>
+          <Search setSearch={handleSetSearch}/> 
+        </Wrapper>  
       </Background>
-      <Overlay show={show} handleSetShow={handleSetShow} data={{}}/>
+      <Overlay data={data} show={show} handleSetShow={handleSetShow} />
+      
     </>
   )
 }
